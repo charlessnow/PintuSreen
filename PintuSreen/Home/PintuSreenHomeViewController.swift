768c3efe867a2fbad65c90392d61e73fe7c75f35
Charles <Charles_snow@gitee.com>
2020-12-11 18:21:48 +0800

    add

---
+++ PintuSreen/Home/PintuSreenHomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PintuSreenHomeViewController.swift
+//  PintuSreen
+//
+//  Created by Charles on 2020/12/11.
+//
+
+import UIKit
+import SnapKit
+
+class PintuSreenHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+    
+    }
+
+}
+
